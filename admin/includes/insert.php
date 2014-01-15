930c9ed7d33dbfdfc475161947f1294eb5580d66
Skemp45 <Shawn@besmartdesigns.com>
2014-01-14 18:28:12 -0700

    Fixed beer insert issue

--- admin/includes/insert.php
+++ admin/includes/insert.php
@@ -3,18 +3,18 @@ session_start();
 if(!isset( $_SESSION['myusername'] )){
 header("location:index.php");
 }
-require 'includes/config.php';
+require 'config.php';
 
 $sql="INSERT INTO beers (name, style, notes, og, fg , srm, ibu, active, tapnumber)
 VALUES
 ('$_POST[name]','$_POST[style]','$_POST[notes]','$_POST[og]','$_POST[fg]','$_POST[srm]','$_POST[ibu]','$_POST[active]','$_POST[tapnumber]')";
 
-if (!mysql_query($sql,$con))
+if (!mysql_query($sql))
   {
   die('Error: ' . mysql_error());
   }
 {
 
-header("location:../add_sucess.php");
+echo "<script>location.href='../beer_main.php';</script>";
 }
 ?>
