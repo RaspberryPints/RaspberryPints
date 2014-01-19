7b29a380209c21fbda1d4dc326b44b47a370df9d
thadius856 <thadius856@gmail.com>
2014-01-18 17:06:24 -0800

    Whitespace fix

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -6,6 +6,7 @@ $db_name="raspberrypints"; // Database name
 $tbl_name="beers";
 
 
+
 //Connect to server and select databse.
 mysql_connect("$host", "$username", "$password")or die("cannot connect to server");
 mysql_select_db("$db_name")or die("cannot select DB");
