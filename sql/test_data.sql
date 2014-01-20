2e38e6f7b689c5a1f3ca18521af2ca404a00029d
Thadius Miller <thadius856@gmail.com>
2014-01-20 15:36:34 -0800

    Updating test_data.sql with additional beers. Why test 10 taps with only 3 beers? ;)

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -28,16 +28,16 @@ USE `raspberrypints`;
 --
 
 INSERT INTO `beers` (`name`, `style`, `notes`, `ogEst`, `fgEst`, `srmEst`, `ibuEst`) VALUES
-('Darth Vader', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
-('OktoberFAST', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
-('Strong Scotch', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
-('Darth Vader2', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
-('OktoberFAST2', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
-('Strong Scotch2', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
-('Darth Vader3', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
-('OktoberFAST3', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
-('Strong Scotch3', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
-('Darth Vader4', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66);
+('Darth Vader', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38.0, 66.0),
+('Strong Scotch', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.018', 17.8, 27.0),
+('Cream of Three Crops', 'Cream Ale', 'Neutral, muted start. Highly carbonated with a soapy head. Unremarkably mild finish of noble hops. Dry and crisp with no distinguishable graininess.', '1.040', '1.009', 2.9, 14.3),
+('Darth Vader', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38.0, 66.0),
+('Haus Pale Ale', 'American Pale Ale', 'Pale straw-gold color with two fingers of fluffy white head. Bread dough and cracker aromas up front, followed immediately by pine and grapefruit. Clean, crisp and dangerously sessionable.', '1.051', '1.011', 5.0, 39.0),
+('Two Hearted', 'American IPA', 'American malts and enormous hop additions give this beer a crisp finish and an incredibly floral aroma.', '1.055', '1.014', 5.6, 52.6),
+('Reaper''s Mild', 'English Mild', 'A full flavored session beer that is both inexpensive to brew and quick to go from grain to glass. Ready to drink in a couple weeks, if you push it.', '1.035', '1.012', 19.1, 20.4),
+('Skeeter Pee', 'Sweet Lemon Wine', 'The original, easy to drink, naturally fermented lemon drink. Bitter, sweet, and a kick in the teeth. This hot-weather thirst quencher puts commercialized lemon-flavored malt beverages to shame.', '1.070', '1.009', 0, 0),
+('Black Peach', 'Iced Tea', 'Black tea infused with the unmistakable summertime flavor of juicy, orchard-fresh peaches and just the right amount of natural milled cane sugar.', '1.000', '1.000', 0, 0),
+('Aloha Morning', 'Hawaiian Punch', 'Children''s strawberry and citrus punch, thinned to suit an adult pallet using only the highest quality dihydrogen monoxide available.', '1.000', '1.000', 0, 0);
 
 
 
