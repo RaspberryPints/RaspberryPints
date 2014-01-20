6416e842138ce3724c9cd6ac8b3c9441fd9b506a
Skemp45 <Shawn@besmartdesigns.com>
2014-01-20 11:25:50 -0700

    update to match new db structure

--- admin/includes/configp.php
+++ admin/includes/configp.php
@@ -1,11 +1,11 @@
 <?php
-		$dbhost     = "localhost";
-		$dbname     = "raspberrypints";
-		$dbuser     = "beer";
-		$dbpass     = "beer";
+		$dbhost     = "TapList45.db.10666578.hostedresource.com";
+		$dbname     = "TapList45";
+		$dbuser     = "TapList45";
+		$dbpass     = "Deer!boy45";
 		$conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
 		
-		$stmt = $conn->prepare('SELECT * FROM config');
+		$stmt = $conn->prepare('SELECT * FROM config WHERE id <= 6');
 		$stmt->execute();
 		$result = $stmt->fetchAll();
 ?>
