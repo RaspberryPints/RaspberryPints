10bd737a91557f339b8a16e5a05283b0099d23f3
Skemp45 <Shawn@besmartdesigns.com>
2014-01-20 12:14:09 -0700

    update to match new db structure

--- admin/update_column.php
+++ admin/update_column.php
@@ -3,20 +3,20 @@ session_start();
 if(!isset( $_SESSION['myusername'] )){
 header("location:index.php");
 }
-require 'includes/config.php';
+require 'includes/conn.php';
 require '../includes/config_names.php';
-require 'includes/congifp.php';
+require 'includes/configp.php';
 
 
 
 // Get values from form 
 $name=$_POST['id'];
-$config_value=$_POST['config_value'];
+$config_Value=$_POST['configValue'];
 
 foreach($_POST as $k => $v){
 	// update data in mysql database
-	$stmt = $conn->prepare("UPDATE config SET config_value=:config_value WHERE id=:id");
-	$stmt->bindParam(':config_value', $v, PDO::PARAM_STR);
+	$stmt = $conn->prepare("UPDATE config SET configValue=:configValue WHERE id=:id");
+	$stmt->bindParam(':configValue', $v, PDO::PARAM_STR);
 	$stmt->bindParam(':id', $k, PDO::PARAM_STR);
 	$result = $stmt->execute();
 }
