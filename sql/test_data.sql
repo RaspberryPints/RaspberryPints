858cdf97033bc8c4d9b8a24a427debeab40bc71c
Jason <JasonUnterman@gmail.com>
2014-01-20 14:06:07 -0800

    removed id from beers data import

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -27,17 +27,17 @@ USE `raspberrypints`;
 -- Dumping data for table `beers`
 --
 
-INSERT INTO `beers` (`id`, `name`, `style`, `notes`, `ogEst`, `fgEst`, `srmEst`, `ibuEst`) VALUES
-(1, 'Darth Vader', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
-(2, 'OktoberFAST', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
-(3, 'Strong Scotch', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
-(4, 'Darth Vader2', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
-(5, 'OktoberFAST2', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
-(6, 'Strong Scotch2', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
-(7, 'Darth Vader3', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
-(8, 'OktoberFAST3', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
-(9, 'Strong Scotch3', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
-(10, 'Darth Vader4', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66);
+INSERT INTO `beers` (`name`, `style`, `notes`, `ogEst`, `fgEst`, `srmEst`, `ibuEst`) VALUES
+('Darth Vader', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
+('OktoberFAST', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
+('Strong Scotch', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
+('Darth Vader2', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
+('OktoberFAST2', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
+('Strong Scotch2', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
+('Darth Vader3', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66),
+('OktoberFAST3', 'Marzen', 'Dark copper body. Thick, chewy, off-white head. Toasty and earthy aromas. Mid-bodied with a semi-bitter, highly-attenuated finish.', '1.051', '1.013', 13, 20),
+('Strong Scotch3', 'Scotch Ale', 'Slightly sweet. Hints of malt and toffee. Finishes with roasted nuts and coffee. Complex and roasty.', '1.074', '1.019', 18, 27),
+('Darth Vader4', 'Cascadian Dark Ale', 'Rich, toasty malt flavor. Generous amounts of pine, citrus and roasted coffee. Herbal aroma with a punch of IPA hops at the finish.', '1.066', '1.016', 38, 66);
 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
