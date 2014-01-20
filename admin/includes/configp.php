4f8dbfa9abdb55aef5e43d8573d99e54cf4881f2
Skemp45 <Shawn@besmartdesigns.com>
2014-01-20 13:58:19 -0700

    remove pw

--- admin/includes/configp.php
+++ admin/includes/configp.php
@@ -1,8 +1,8 @@
 <?php
-		$dbhost     = "TapList45.db.10666578.hostedresource.com";
-		$dbname     = "TapList45";
-		$dbuser     = "TapList45";
-		$dbpass     = "Deer!boy45";
+		$dbhost     = "localhost";
+		$dbname     = "raspberrypints";
+		$dbuser     = "beer";
+		$dbpass     = "beer";
 		$conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
 		
 		$stmt = $conn->prepare('SELECT * FROM config WHERE id <= 6');
