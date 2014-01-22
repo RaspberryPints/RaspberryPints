a56c6e0adaf4ad4e6e6e766685b690a70713e6ca
Skemp45 <Shawn@besmartdesigns.com>
2014-01-22 07:23:31 -0700

    used php includes

--- admin/beer_main.php
+++ admin/beer_main.php
@@ -11,26 +11,19 @@ require 'includes/conn.php';
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>RaspberryPints-Add</title>
+<title>RaspberryPints</title>
 <link href="styles/layout.css" rel="stylesheet" type="text/css" />
 <link href="styles/wysiwyg.css" rel="stylesheet" type="text/css" />
 <!-- Theme Start -->
 <link href="styles.css" rel="stylesheet" type="text/css" />
 <!-- Theme End -->
 <link href='http://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
-<style>
-
-#welcome
-{ font-family: 'Fredoka One', cursive; font-weight: 400; color: white; font-size:140% }
-
-</style>
-
-
 </head>
-<div id="header">
-    	<br />&nbsp &nbsp <input type="button" value="My Account" class="btn" onClick="location. href='Mya.php'" />
-<input type="button" value="Logout" class="btn" onClick="location. href='includes/endses.php'"/> <a href="personalize.php" title="personalize"><img src="img/icons/gear.png" height="30" width="30" align="right"></a>&nbsp;<a href="admin.php" title="home"><img src="img/icons/home.png" height="30" width="30" align="right"></a>
-    </div>
+	<!-- Start Header  -->
+<?
+include 'header.php';
+?>
+	<!-- End Header -->
         
     <!-- Top Breadcrumb Start -->
     <div id="breadcrumb">
@@ -48,75 +41,11 @@ require 'includes/conn.php';
     <div id="rightside">
 
 	
-		 <div class="contentcontainer med left">
-            	<p>
-<?php
-require 'includes/config.php';
-
-$sql="SELECT * FROM $tbl_name WHERE active = true";
-$result=mysql_query($sql);
-?>	
-<div class="headings alt">
-                <h2>Beer List</h2>
+		<div class="contentcontainer med left">
+            <div class="headings alt">
+                <h2>Add Or Remove Beer</h2>
             </div>
-			<br />
-			<h1><u>Active Beers</u></h1>
-<table width="400" border="0" cellspacing="0" cellpadding="0">
-<tr>
-<td>
-<table width="400" border="0" cellspacing="0" cellpadding="3">
-<?php
-while($rows=mysql_fetch_array($result)){
-?>
-
-<tr>
-<td><b><? echo $rows['name']; ?></b></td>
-<td align="center"><a href="update.php?beerid=<? echo $rows['beerid']; ?>"><img src="img/icons/pencil.png" height="30" width="30"></a></td>
-<tr><td><hr></td></tr>
-
-<?php
-}
-?>
-
-</table>
-</td>
-</tr>
-</table></p>
-
-			<br />
-			<h1><u>Inactive Beers</u></h1>
-			
-			
-			
-			<?php
-require 'includes/config.php';
-
-$sql="SELECT * FROM $tbl_name WHERE active='2'";
-$result=mysql_query($sql);
-?>	
-
-<table width="400" border="0" cellspacing="0" cellpadding="0">
-<tr>
-<td>
-<table width="400" border="0" cellspacing="0" cellpadding="3">
-<?php
-while($rows=mysql_fetch_array($result)){
-?>
-
-<tr>
-<td><b><? echo $rows['name']; ?></b></td>
-<td align="center"><a href="update.php?beerid=<? echo $rows['beerid']; ?>"><img src="img/icons/pencil.png" height="30" width="30"></a></td>
-<tr><td><hr></td></tr>
-
-<?php
-}
-?>
-</table>
-</td>
-</tr>
-</table>
-<br />
-			<h1><u>Add A Beers</u></h1>
+            <div class="contentbox">
 			<form action="includes/insert.php" method="post">
 <label for="textfield"><strong>Beer Name:</strong></label> <input class="inputbox" type="text" name="name" required> <br />
 <label for="textfield"><strong>Style:</strong></label> <input class="inputbox" type="text" name="style"> <br />
@@ -125,77 +54,29 @@ while($rows=mysql_fetch_array($result)){
 <label for="textfield"><strong>FG:</strong></label> <input class="inputbox" type="text" name="fg"> <br />
 <label for="textfield"><strong>SRM:</strong></label> <input class="inputbox" type="text" name="srm"> <br />
 <label for="textfield"><strong>IBU's:</strong></label> <input class="inputbox" type="text" name="ibu"> <br />
-<label for="textfield"><strong>Active (1=Yes 2=No):</strong></label> <input class="inputbox" type="text" name="active"> <br />
-<label for="textfield"><strong>Tap Number:</strong></label> <input class="inputbox" type="text" name="tapnumber"> <br />
 <input class="btn" type="submit" value="Add Beer">
 </form>
      </div>
-        <div id="footer">
-        	&copy; Copyright 2012-2014 RaspberryPints
-        </div> 
-          
+    <!-- Start Footer -->   
+<? 
+include 'footer.php';
+?>
+
+	<!-- End Footer -->
+           </div>
     </div>
     <!-- Right Side/Main Content End -->
     
-        <!-- Left Dark Bar Start -->
-    <div id="leftside">
-<div id="welcome"> &nbsp &nbsp Hello: &nbsp
-  <?php
-  
-  $sql="SELECT `name` FROM `users` WHERE username='$_SESSION[myusername]'";
-  $result=mysql_query($sql);
-
-echo mysql_result($result, 0, 'name');
-?></div>
-    	<div class="user">
-        	<a href="../"><img src="img/logo.png" width="120" height="120" class="hoverimg" alt="Avatar" /></a>
-			</div>
-
-        
-        <ul id="nav">
-        	<li>
-                <ul class="navigation">
-                    <li class="heading selected">Welcome</li>
-                </ul>			 <li>
-			 <ul class="navigation">
-                    <li><a href="admin.php" title="Update">Home</a></li>
-                </ul>
-            <li>
-                <a class="expanded heading">Configure</a>
-                 <ul class="navigation">
-                    <li><a href="beer_main.php" title="beer-list">Beer List</a></li>
-					<li><a href="tap_list.php" title="tap-list">Tap List</a></li>
-					<li><a href="personalize.php" title="personalize">Personalize</a></li>
-                </ul>
-            </li>
-			 <li>
-                <a class="expanded heading">Analytics</a>
-                 <ul class="navigation">
-                    <li><a href="#" target="" title="temperature">Temperature Monitoring</a>Comming soon</li>
-                    <li><a href="#" title="GPT">Gallons Per Tap</a>Comming soon</li>
-                    <li><a href="#" title="rank">Beer Rank</a>Comming soon</li>
-                </ul>
-            </li>
-			            <li>
-                <a class="expanded heading">Other Help</a>
-                 <ul class="navigation">
-					<li><a href="#" title="faq">F.A.Q</a></li>
-					<li><a href="report_bug.php" title="faq">Report Bug</a></li>
-					<li><a href="feedback.php" title="faq">FeedBack</a></li>					
-                </ul>
-            </li>          
-        </ul>
-    </div>
-    <!-- Left Dark Bar End --> 
-    
-    <script type="text/javascript" src="js/enhance.js"></script>	
-    <script type='text/javascript' src='js/excanvas.js'></script>
-	<script type='text/javascript' src='js/jquery.min.js'></script>
-    <script type='text/javascript' src='js/jquery-ui.min.js'></script>
-	<script type='text/javascript' src='scripts/jquery.wysiwyg.js'></script>
-    <script type='text/javascript' src='scripts/visualize.jQuery.js'></script>
-    <script type="text/javascript" src='scripts/functions.js'></script>
-    
+	<!-- Start Left Bar Menu -->   
+<? 
+include 'left_bar.php';
+?>
+	<!-- End Left Bar Menu -->  
+	<!-- Start Js  -->
+<?
+include 'scripts.php';
+?>
+	<!-- End Js -->
     <!--[if IE 6]>
     <script type='text/javascript' src='scripts/png_fix.js'></script>
     <script type='text/javascript'>
