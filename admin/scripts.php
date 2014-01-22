645ff8a6a4f1d1dfb3edb8953be3a99f3503f4c3
Skemp45 <Shawn@besmartdesigns.com>
2014-01-22 07:26:25 -0700

    used php includes

---
+++ admin/scripts.php
@@ -0,0 +1,8 @@
+	<script type="text/javascript" src="js/enhance.js"></script>	
+    <script type='text/javascript' src='js/excanvas.js'></script>
+	<script type='text/javascript' src='js/jquery.min.js'></script>
+    <script type='text/javascript' src='js/jquery-ui.min.js'></script>
+	<script type='text/javascript' src='scripts/jquery.wysiwyg.js'></script>
+    <script type='text/javascript' src='scripts/visualize.jQuery.js'></script>
+    <script type="text/javascript" src='scripts/functions.js'></script>
+    
