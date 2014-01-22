7286f3b33d64b4782283561d8c9ab48a38614143
Skemp45 <Shawn@besmartdesigns.com>
2014-01-22 07:24:59 -0700

    used php includes

--- admin/tap_list.php
+++ admin/tap_list.php
@@ -11,26 +11,19 @@ require 'includes/conn.php';
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>RaspberryPints-Add</title>
+<title>RaspberryPints</title>
 <link href="styles/layout.css" rel="stylesheet" type="text/css" />
 <link href="styles/wysiwyg.css" rel="stylesheet" type="text/css" />
-<!-- Theme Start -->
+	<!-- Theme Start -->
 <link href="styles.css" rel="stylesheet" type="text/css" />
-<!-- Theme End -->
+	<!-- Theme End -->
 <link href='http://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
-<style>
-
-#welcome
-{ font-family: 'Fredoka One', cursive; font-weight: 400; color: white; font-size:140% }
-
-</style>
-
-
 </head>
-<div id="header">
-    	<br />&nbsp &nbsp <input type="button" value="My Account" class="btn" onClick="location. href='Mya.php'" />
-<input type="button" value="Logout" class="btn" onClick="location. href='includes/endses.php'"/> <a href="personalize.php" title="personalize"><img src="img/icons/gear.png" height="30" width="30" align="right"></a>&nbsp;<a href="admin.php" title="home"><img src="img/icons/home.png" height="30" width="30" align="right"></a>
-    </div>
+	<!-- Start Header  -->
+<?
+include 'header.php';
+?>
+	<!-- End Header -->
         
     <!-- Top Breadcrumb Start -->
     <div id="breadcrumb">
@@ -46,112 +39,64 @@ require 'includes/conn.php';
      
     <!-- Right Side/Main Content Start -->
     <div id="rightside">
-
-	
 		 <div class="contentcontainer med left">
-            	<p>
-
-
-
+	<p>
+	<!-- Set Tap Number Form -->
+		<form action="" method="POST" name="taplimit">
+			<b>Number Of Taps:</b> &nbsp <input type="text" class="xsmallbox"> &nbsp <input type="submit" class="btn" value="Update" />
+		</form>
+	</p>
+	<!-- End Tap Number Form -->
 <br />
-<p><b>Number Of Taps:</b> &nbsp <input type="text" class="smallbox"> &nbsp <input type="submit" class="btn" value="Update" /></p><br />
-</b></p>
+	<!-- Start On Tap Section -->
+		<table width="950" border="0" cellspacing="0" cellpadding="0">
+			<thead>
+			<tr>
+			<th>
+				<b>Tap #</b>
+			</th>
+			<th>
+				<b>Beer Name</b>
+			</th>
+			<th>
+				<b>Vitals This Batch</b>
+			</th>
+			<th>
+				<b>Keg Info</b>
+			</th>
+			</tr>
+			</thead>
+		<tbody>
 
-
-
-<table width="950" border="0" cellspacing="0" cellpadding="0">
-<thead>
-<tr>
-<th>
-<b>Tap #</b>
-</th>
-<th>
-<b>Beer Name</b>
-</th>
-<th>
-<b>Vitals This Batch</b>
-</th>
-<th>
-<b>Keg Info</b>
-</th>
-</tr>
-</thead>
-<tbody>
-
-
-</tbody>
-</table>
+		</tbody>
+		</table>
 <br />
-<div align="right">
-<input type="submit" class="btn" value="Save" /> &nbsp &nbsp <input type="submit" class="btn" value="Reset" />
-</div>
+		<div align="right">
+			<input type="submit" class="btn" value="Save" /> &nbsp &nbsp <input type="submit" class="btn" value="Reset" />
+		</div>
+		
         </div>
-        <div id="footer">
-        	&copy; Copyright 2012-2014 RaspberryPints
-        </div> 
+	<!-- End On Tap Section -->
+
+    <!-- Start Footer -->   
+<? 
+include 'footer.php';
+?>
+
+	<!-- End Footer -->
           
     </div>
     <!-- Right Side/Main Content End -->
-    
-        <!-- Left Dark Bar Start -->
-    <div id="leftside">
-<div id="welcome"> &nbsp &nbsp Hello: &nbsp
-  <?php
-  
-  $sql="SELECT `name` FROM `users` WHERE username='$_SESSION[myusername]'";
-  $result=mysql_query($sql);
-
-echo mysql_result($result, 0, 'name');
-?></div>
-    	<div class="user">
-        	<a href="../"><img src="img/logo.png" width="120" height="120" class="hoverimg" alt="Avatar" /></a>
-			</div>
-
-        
-             <ul id="nav">
-        	<li>
-                <ul class="navigation">
-                    <li class="heading selected">Welcome</li>
-                </ul>			 <li>
-			 <ul class="navigation">
-                    <li><a href="admin.php" title="Update">Home</a></li>
-                </ul>
-            <li>
-                <a class="expanded heading">Configure</a>
-                 <ul class="navigation">
-                    <li><a href="beer_main.php" title="beer-list">Beer List</a></li>
-					<li><a href="tap_list.php" title="tap-list">Tap List</a></li>
-					<li><a href="personalize.php" title="personalize">Personalize</a></li>
-                </ul>
-            </li>
-			 <li>
-                <a class="expanded heading">Analytics</a>
-                 <ul class="navigation">
-                    <li><a href="#" target="" title="temperature">Temperature Monitoring</a>Coming soon</li>
-                    <li><a href="#" title="GPT">Gallons Per Tap</a>Coming soon</li>
-                    <li><a href="#" title="rank">Beer Rank</a>Coming soon</li>
-                </ul>
-            </li>
-			            <li>
-                <a class="expanded heading">Other Help</a>
-                 <ul class="navigation">
-					<li><a href="#" title="faq">F.A.Q</a></li>
-					<li><a href="report_bug.php" title="faq">Report Bug</a></li>
-					<li><a href="feedback.php" title="faq">FeedBack</a></li>					
-                </ul>
-            </li>          
-        </ul>
-    </div>
-    <!-- Left Dark Bar End --> 
-    
-    <script type="text/javascript" src="js/enhance.js"></script>	
-    <script type='text/javascript' src='js/excanvas.js'></script>
-	<script type='text/javascript' src='js/jquery.min.js'></script>
-    <script type='text/javascript' src='js/jquery-ui.min.js'></script>
-	<script type='text/javascript' src='scripts/jquery.wysiwyg.js'></script>
-    <script type='text/javascript' src='scripts/visualize.jQuery.js'></script>
-    <script type="text/javascript" src='scripts/functions.js'></script>
-    
+	<!-- Start Left Bar Menu -->   
+<? 
+include 'left_bar.php';
+?>
+	<!-- End Left Bar Menu -->  
+	<!-- Start Js  -->
+<?
+include 'scripts.php';
+?>
+	<!-- End Js -->
     <!--[if IE 6]>
     <script type='text/javascript' src='scripts/png_fix.js'></script>
     <script type='text/javascript'>
