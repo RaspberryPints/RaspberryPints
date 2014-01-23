d7ac4b01b353eb0f272160e490d5b53311a31d18
Thadius Miller <thadius856@gmail.com>
2014-01-22 18:52:55 -0800

    Added 4k support for RPints logo and brewery logo.

--- index.php
+++ index.php
@@ -69,13 +69,21 @@
         	<!-- Header with Brewery Logo and Project Name -->
             <div class="header clearfix">
                 <div class="HeaderLeft">
-                   <a href="admin/admin.php"><img src="<?php echo $config[ConfigNames::LogoUrl]; ?>" height="100" alt=""></a>
+					<?php if($config[ConfigNames::UseHighResolution]) { ?>			
+						<a href="admin/admin.php"><img src="<?php echo $config[ConfigNames::LogoUrl]; ?>" height="200" alt=""></a>
+					<?php } else { ?>
+						<a href="admin/admin.php"><img src="<?php echo $config[ConfigNames::LogoUrl]; ?>" height="100" alt=""></a>
+					<?php } ?>
                 </div>
                 <div class="HeaderCenter">
                     <h1 id="HeaderTitle"><? echo $config[ConfigNames::HeaderText]; ?></h1>
                 </div>
                 <div class="HeaderRight">
-                   <a href="https://github.com/raspberrypints/raspberrypints"><img src="img/RaspberryPints.png" height="100" alt=""></a>
+					<?php if($config[ConfigNames::UseHighResolution]) { ?>			
+						<a href="http://www.raspberrypints.com"><img src="img/RaspberryPints-4k.png" height="200" alt=""></a>
+					<?php } else { ?>
+						<a href="http://www.raspberrypints.com"><img src="img/RaspberryPints.png" height="100" alt=""></a>
+					<?php } ?>
                 </div>
             </div>
             <!-- End Header Bar -->
