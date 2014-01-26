8ded9beeecea227b52c8bd406a835571821dac76
Thadius Miller <thadius856@gmail.com>
2014-01-25 16:57:07 -0800

    - Added preview of logo and bg on personalize.php with RPi pink border - Added future navbar entries to left_bar.php - Revised phrasing and tabbing on both pages.

--- admin/left_bar.php
+++ admin/left_bar.php
@@ -1,53 +1,88 @@
-    <!-- Left Dark Bar Start -->
-    <div id="leftside">
-	<!-- Start User Echo -->
-<div id="welcome"> &nbsp &nbsp Hello: &nbsp
-  <?php
-  
-  $sql="SELECT `name` FROM `users` WHERE username='$_SESSION[myusername]'";
-  $result=mysql_query($sql);
+<!-- Left Dark Bar Start -->
+<div id="leftside">
 
-echo mysql_result($result, 0, 'name');
-?></div>
-	<!-- End User Echo -->
-    	<div class="user">
-        	<a href="../"><img src="img/logo.png" width="120" height="120" class="hoverimg" alt="Avatar" /></a>
-			</div>
+<!-- Start User Echo -->
+<div id="welcome"> &nbsp; Welcome, <br />
+	&nbsp;
+	<?php
+		$sql="SELECT `name` FROM `users` WHERE username='$_SESSION[myusername]'";
+		$result=mysql_query($sql);
+		echo mysql_result($result, 0, 'name');
+	?>
+</div>
 
-	<!-- Start Navagation -->
-             <ul id="nav">
-        	<li>
-                <ul class="navigation">
-                    <li class="heading selected">Welcome</li>
-                </ul>			 <li>
-			 <ul class="navigation">
-                    <li><a href="admin.php" title="Update">Home</a></li>
-                </ul>
-            <li>
-                <a class="expanded heading">Configure</a>
-                 <ul class="navigation">
-                    <li><a href="beer_main.php" title="beer-list">Beer List</a></li>
-					<li><a href="tap_list.php" title="tap-list">Tap List</a></li>
-					<li><a href="personalize.php" title="personalize">Personalize</a></li>
-                </ul>
-            </li>
-			 <li>
-                <a class="collapsed heading">Analytics-<small> Coming Soon</small></a>
-                 <ul class="navigation">
-                    <li><a href="#" target="" title="temperature">Temperature Monitoring</a>Coming soon</li>
-                    <li><a href="#" title="GPT">Gallons Per Tap</a>Coming soon</li>
-                    <li><a href="#" title="rank">Beer Rank</a>Coming soon</li>
-                </ul>
-            </li>
-			            <li>
-                <a class="expanded heading">Other Help</a>
-                 <ul class="navigation">
-					<li><a href="#" title="faq">F.A.Q</a></li>
-					<li><a href="report_bug.php" title="faq">Report Bug</a></li>
-					<li><a href="feedback.php" title="faq">FeedBack</a></li>					
-                </ul>
-            </li>          
-        </ul>
-    </div>
-	<!-- End Navagation -->
-    <!-- Left Dark Bar End --> 
+<!-- End User Echo -->
+<div class="user">
+	<a href="../"><img src="img/logo.png" width="120" height="120" class="hoverimg" alt="Avatar" /></a>
+</div>
+
+<!-- Start Navagation -->
+<ul id="nav">
+	<li>
+		<ul class="navigation">
+			<li class="heading selected">Welcome</li>
+		</ul>
+	<li>
+	<li>
+		<a class="expanded heading">Basic Setup</a>
+		<ul class="navigation">
+			<li><a href="beer_main.php" title="beer-list">Beer Recipes</a></li>
+			<li><a href="#" title="keg-detail">Kegs Details <small>(coming soon)</small></a></li>
+			<li><a href="tap_list.php" title="tap-list">Edit Taplist</a></li>
+		</ul>
+	</li>
+		<li>
+		<a class="expanded heading">Personalization</a>
+		<ul class="navigation">
+			<li><a href="personalize.php" title="personalize">Show/Hide Columns</a></li>
+			<li><a href="personalize.php" title="personalize">Headers</a></li>
+			<li><a href="personalize.php" title="personalize">Brewery Logo</a></li>
+			<li><a href="personalize.php" title="personalize">Background Image</a></li>
+			<li><a href="#" title="theme">Themes <small>(coming v3.0.0)</small></a></li>
+			<li><a href="#" title="drinker-acct">Drinker Accounts <small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="personalize">Units of Measure <small>(coming v3.0.0)</small></a></li>
+		</ul>
+	</li>
+	<li>
+		<a class="collapsed heading">Advanced Hardware</a>
+		<ul class="navigation">
+			<li><a href="#" title="gpio-pin-layout">GPIO Pin Layout<small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="flow-calibration">Flow Meters <small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="rfid-reader">RFID Readers <small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="temp-probe">Temperature Probes <small>(coming v3.0.0)</small></a></li>
+			<li><a href="#" title="solenoid">Solenoids <small>(coming v3.0.0)</small></a></li>
+			<li><a href="#" title="motion-sensor">Motion Sensors <small>(coming v3.0.0)</small></a></li>
+		</ul>
+	</li>
+	<li>
+		<a class="collapsed heading">Analytics</a>
+		<ul class="navigation">
+			<li><a href="#" title="temperature-vs-time">Temperature vs Time <small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="pour-history">Pour history <small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="tap-history">Tap history <small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="rank">Beer statistics <small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="drinker-stats">Drinker statistics <small>(coming v2.0.0)</small></a></li>
+			<li><a href="#" title="GPT">Tap statistics <small>(coming v2.0.0+)</small></a></li>
+		</ul>
+	</li>
+	<li>
+		<a class="expanded heading">Help</a>
+		<ul class="navigation">
+			<li><a href="#" title="faq">F.A.Q. <small>(coming soon)</small></a></li>
+			<li><a href="report_bug.php" title="faq">Report a Bug</a></li>
+			<li><a href="feedback.php" title="faq">Request a Feature</a></li>
+		</ul>	
+	</li>
+	<li>
+		<a class="expanded heading">Everything Else</a>
+		<ul class="navigation">
+			<li><a href="http://www.raspberrypints.com/" title="faq">Official Website <small>(coming soon)</small></a></li>
+			<li><a href="#" title="about">About <small>(coming soon)</small></a></li>
+			<li><a href="#" title="contributors">Contributors <small>(coming soon)</small></a></li>
+			<li><a href="#" title="licensing">Licensing <small>(coming soon)</small></a></li>
+		</ul>
+	</li>
+</ul>
+
+<!-- End Navagation -->
+<!-- Left Dark Bar End --> 
--- admin/personalize.php
+++ admin/personalize.php
@@ -22,89 +22,97 @@ require 'includes/configp.php';
 <!-- Theme End -->
 <link href='http://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
 </head>
-	<!-- Start Header  -->
+<!-- Start Header  -->
 <?
 include 'header.php';
 ?>
-	<!-- End Header -->
-        
-    <!-- Top Breadcrumb Start -->
-    <div id="breadcrumb">
-    	<ul>	
-        	<li><img src="img/icons/icon_breadcrumb.png" alt="Location" /></li>
-        	<li><strong>Location:</strong></li>
-            <li><a href="#" title="">Sub Section</a></li>
-            <li>/</li>
-            <li class="current">Configure Settings</li>
-        </ul>
-    </div>
-    <!-- Top Breadcrumb End -->
-     
-    <!-- Right Side/Main Content Start -->
-    <div id="rightside">
-
+<!-- End Header -->
 	
-		 <div class="contentcontainer med left">
-            <div class="headings alt">
-                <h2>Configure Your Tap List Display Settings</h2>
-            </div>
-            <div class="contentbox">
-			<h2>Enable/Disable Features</h2><br /> 
-			<form method="post" action="update_column.php">
-<?php
-foreach($result as $row) {
-echo '<h3>' . $row['displayName'] . '</h3>' .
-'On<input type="radio" ' . ($row['configValue']?'checked':'') . ' name="' . str_replace(' ','',$row['id']) . '" value="1">' .
-'Off<input type="radio" ' . (!$row['configValue']?'checked':'') . ' name="' . str_replace(' ','',$row['id']) . '" value="0"><br>' .
-'<br>';
- }
+<!-- Top Breadcrumb Start -->
+<div id="breadcrumb">
+	<ul>	
+		<li><img src="img/icons/icon_breadcrumb.png" alt="Location" /></li>
+		<li><strong>Location:</strong></li>
+		<li><a href="#" title="">Sub Section</a></li>
+		<li>/</li>
+		<li class="current">Configure Settings</li>
+	</ul>
+</div>
+<!-- Top Breadcrumb End -->
 
-?>
-<input type="submit" class="btn" value="Save" />
-</form>
-<hr />
+<!-- Right Side/Main Content Start -->
+<div id="rightside">
+	<div class="contentcontainer med left">
+		<div class="headings alt">
+			<h2>Personalize </h2>
+		</div>
+		<div class="contentbox">
+		<h2>Show/Hide Columns</h2><br /> 
+		<form method="post" action="update_column.php">
 			<?php
-			$sql="SELECT configValue FROM config WHERE configName ='".ConfigNames::HeaderText."'";
-$result=mysql_query($sql);
-$headerText=mysql_fetch_array($result);?>
+				foreach($result as $row) {
+				echo '<h3>' . $row['displayName'] . ":" . '</h3>' . 
+					'On<input type="radio" ' . ($row['configValue']?'checked':'') . ' name="' . str_replace(' ','',$row['id']) . '" value="1">' .
+					'Off<input type="radio" ' . (!$row['configValue']?'checked':'') . ' name="' . str_replace(' ','',$row['id']) . '" value="0"><br>' .
+					'<br>';
+			} ?>
+			<input type="submit" class="btn" value="Save" />
+		</form>
 
-<h2>Change Header Text</h2>
-			<form method="post" action="update_header_text.php"><br />
-             <b>Header text</b><br />
-			<input type="text" class="mediumbox" value="<? echo $headerText['configValue']; ?>" name="header_text"> &nbsp <br />
-			<input type="submit" class="btn" name="Submit" value="Submit">
-			</form>
-		<hr />	
-		<form enctype="multipart/form-data" action="update_logo.php" method="POST">
-		<br />
-		<h2>Change Site And Admin Logo</h2><br /><input name="uploaded" type="file" accept="image/gif, image/jpg, image/png"/><br /><br />
-		<input type="submit" class="btn" value="Upload" />
-		</form> 
 		<hr />
-		<form enctype="multipart/form-data" action="update_background.php" method="POST">
-		<br />
-		<h2>Change Background Image</h2><br /><input name="uploaded" type="file" accept="image/gif, image/jpg, image/png"/><br /><br />
-		<input type="submit" class="btn" value="Upload" /><br /><br />
-		</form> 
-		<form action="restore_background.php" method="POST">
-<hr />
-		<input type="submit" class="btn" value="Restore Default Background" />
-		</form> 
-		<br />
-		
-            </div>
 
-        </div>
-    <!-- Start Footer -->   
+		<?php
+			$sql="SELECT configValue FROM config WHERE configName ='".ConfigNames::HeaderText."'";
+			$result=mysql_query($sql);
+			$headerText=mysql_fetch_array($result);
+		?>
+
+		<h2>Header Text</h2>
+		<p>This text appears at the very top of the taplist.</p>
+			<form method="post" action="update_header_text.php">
+				<input type="text" class="mediumbox" value="<? echo $headerText['configValue']; ?>" name="header_text"> &nbsp 
+				<input type="submit" class="btn" name="Submit" value="Submit">
+			</form>
+
+		<hr />
+
+		<h2>Brewery Logo</h2>
+		<p>This logo appears on the taplist and the admin panel.</p>
+			<b>Current image:</b><br /><br />
+				<img src="img/logo.png" height="100" alt="Brewery Logo" style="border-style: solid; border-width: 2px; border-color: #d6264f;" />
+			<form enctype="multipart/form-data" action="update_logo.php" method="POST"><br />
+				<input name="uploaded" type="file" accept="image/gif, image/jpg, image/png"/>
+				<input type="submit" class="btn" value="Upload" />
+			</form> 
+
+		<hr />
+
+		<h2>Background Image</h2>
+		<p>This background appears on the taplist.</p>
+			<b>Current image:</b><br /><br />
+				<img src="img/background.jpg" width="200" alt="Background" style="border-style: solid; border-width: 2px; border-color: #d6264f;" />
+			<form enctype="multipart/form-data" action="update_background.php" method="POST">
+				<input name="uploaded" type="file" accept="image/gif, image/jpg, image/png"/>
+				<input type="submit" class="btn" value="Upload" /><br /><br />
+			</form>
+			<form action="restore_background.php" method="POST">
+				<input type="submit" class="btn" value="Restore Default Background" />
+			</form> 
+
+	</div>
+</div>
+
+<!-- Start Footer -->
+
 <? 
 include 'footer.php';
 ?>
 
 	<!-- End Footer -->
-          
-    </div>
-    <!-- Right Side/Main Content End -->
-    
+		
+	</div>
+	<!-- Right Side/Main Content End -->
+	
 	<!-- Start Left Bar Menu -->   
 <? 
 include 'left_bar.php';
@@ -115,11 +123,11 @@ include 'left_bar.php';
 include 'scripts.php';
 ?>
 	<!-- End Js -->
-    <!--[if IE 6]>
-    <script type='text/javascript' src='scripts/png_fix.js'></script>
-    <script type='text/javascript'>
-      DD_belatedPNG.fix('img, .notifycount, .selected');
-    </script>
-    <![endif]--> 
+	<!--[if IE 6]>
+	<script type='text/javascript' src='scripts/png_fix.js'></script>
+	<script type='text/javascript'>
+	DD_belatedPNG.fix('img, .notifycount, .selected');
+	</script>
+	<![endif]--> 
 </body>
 </html>
