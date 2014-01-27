da438770daae7b702d8bae14768f6390b577dd7d
Skemp45 <Shawn@besmartdesigns.com>
2014-01-27 07:50:09 -0700

    added add beer

--- admin/includes/insert.php
+++ admin/includes/insert.php
@@ -4,12 +4,13 @@ if(!isset( $_SESSION['myusername'] )){
 	//header("location:index.php");
 }
 require 'conn.php';
+require_once '../includes/functions.php';
 
 
-$sql="INSERT INTO beers (name, style, notes, ogEst, fgEst, srmEst, ibuEst, createdDate, modifiedDate) VALUES ('$_POST[name]','$_POST[style]','$_POST[notes]','$_POST[og]','$_POST[fg]','$_POST[srm]','$_POST[ibu]', NOW(), NOW())";
+$sql="INSERT INTO beers (name, style, notes, ogEst, fgEst, srmEst, ibuEst, modifiedDate) VALUES ('$_POST[name]','$_POST[style]','$_POST[notes]','$_POST[ogEst]','$_POST[fgEst]','$_POST[srmEst]','$_POST[ibuEst]', NOW())";
 
 if (!mysql_query($sql)) {
   die('Error: ' . mysql_error());
 }
 
-header("Location: ../beer_main.php");
+	redirect('../beer_main.php');
