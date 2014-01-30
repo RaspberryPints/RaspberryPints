605ae302e4fd3184af35964bd8e2c263e7320ed7
mtnbound <ejNuh8ralj>
2014-01-30 14:42:15 -0500

    Cleanup

--- install/includes/configprocessor.php
+++ install/includes/configprocessor.php
@@ -9,11 +9,8 @@
 ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
-<<<<<<< HEAD
 require_once __DIR__.'/sql_parse.php';
 
-=======
->>>>>>> master
 //Process and load form data
 $servername = $_POST["servername"];
 $rootpass = $_POST["rootpass"];
@@ -24,11 +21,7 @@ $adminuser = $_POST["adminuser"];
 $adminpass1 = $_POST["adminpass1"];
 $adminpass2 = $_POST["adminpass2"];
 $action = $_POST["selectaction"];
-<<<<<<< HEAD
 //$sampledata = $_POST["sampledata"];
-=======
-$sampledata = $_POST["sampledata"];
->>>>>>> master
 
 //Create the MD5 hash value for the admin password
 $adminhash = md5($adminpass1);
@@ -62,10 +55,7 @@ if (mysqli_connect_errno())
   $validerror .= "<br><strong>Cannot connect the the database using the supplied information.</strong>";
   }
 //##TODO## Validate that there is no raspberrypints DB (not an upgrade)
-<<<<<<< HEAD
 //##TODO## Check if administrator account already exists
-=======
->>>>>>> master
 
 echo "Done<br>";
 flush();
@@ -102,7 +92,6 @@ if ($action == 'remove')
 if ($action == 'install')
 {
 	
-<<<<<<< HEAD
 require_once __DIR__.'/config_files.php';
 	
 	//-----------------Create the main config file-----------------
@@ -110,11 +99,6 @@ require_once __DIR__.'/config_files.php';
 	flush();
 	
 	file_put_contents('../../includes/config.php', $mainconfigstring);
-=======
-	//##TODO## -----------------Create the main config file-----------------
-	echo "Update config files...";
-	flush();
->>>>>>> master
 
 	echo "Done<br>";
 	flush();
@@ -122,13 +106,8 @@ require_once __DIR__.'/config_files.php';
 	echo "Update admin config files...";
 	flush();
 
-<<<<<<< HEAD
 	file_put_contents('../../admin/includes/conn.php', $adminconfig1);
 	file_put_contents('../../admin/includes/configp.php', $adminconfig2);
-=======
-	echo "Done<br>";
-	flush();
->>>>>>> master
 
 	//-----------------Create RPints User--------------------------
 	echo "Creating RPints database user...";
@@ -150,7 +129,6 @@ require_once __DIR__.'/config_files.php';
 	//-----------------Run The Schema File-------------------------
 	echo "Running Database Script...";
 	flush();
-<<<<<<< HEAD
 	$dbms_schema = "../../sql/schema.sql";
 
 		
@@ -170,10 +148,6 @@ require_once __DIR__.'/config_files.php';
 	mysql_query($sql) or die('error in query');
 	}
 
-=======
-	$command = "mysql -uroot -p".$rootpass . " -h " . $servername . " < /var/www/sql/schema.sql";
-	$output = shell_exec($command);
->>>>>>> master
 	echo "Done<br>";
 	flush();
 
@@ -189,11 +163,7 @@ require_once __DIR__.'/config_files.php';
 	  }
 	$currentdate = Date('Y-m-d H:i:s');
 	$sql = "INSERT INTO users (username, password, name, email, createdDate, modifiedDate) VALUES ('" . $adminuser . "','" . $adminhash . "','name','email','" . $currentdate . "','" . $currentdate . "');";
-<<<<<<< HEAD
 	echo "<br>" . $sql . "<br>";
-=======
-
->>>>>>> master
 	$result = mysqli_query($con,$sql);
 	mysqli_close($con);
 	echo "Done<br>";
@@ -213,18 +183,7 @@ require_once __DIR__.'/config_files.php';
 		}
 }
 
-<<<<<<< HEAD
 
-=======
-/*
-if ($key != '' && $color != '') {
-    $f = fopen('config.php', 'w') or die("can't open file");
-    fwrite($f, '<?php $keyword=' . $key . ';$color=' . $color . ';?>');
-    fclose($f);
-} else { // write default values or show an error message }
-
-*/
->>>>>>> master
 
 ##TODO## On Success - redirect to /index.php
 
--- install/index.php
+++ install/index.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="includes/jquery-2.1.0.min.js"></script>
+
 
 <html>
 <script>
@@ -27,157 +27,33 @@ $(document).ready(function () {
    $("#txtConfirmAdminPassword").keyup(checkAdminPasswordMatch);
 });
     </script>
-<<<<<<< HEAD
-<script>
-$.validator.setDefaults({
-	submitHandler: function() { alert("submitted!"); }
-});
 
-$().ready(function() {
-	// validate the comment form when it is submitted
-	$("#commentForm").validate();
 
-	// validate signup form on keyup and submit
-	$("#signupForm").validate({
-		rules: {
-			firstname: "required",
-			lastname: "required",
-			username: {
-				required: true,
-				minlength: 2
-			},
-			password: {
-				required: true,
-				minlength: 5
-			},
-			confirm_password: {
-				required: true,
-				minlength: 5,
-				equalTo: "#password"
-			},
-			email: {
-				required: true,
-				email: true
-			},
-			topic: {
-				required: "#newsletter:checked",
-				minlength: 2
-			},
-			agree: "required"
-		},
-		messages: {
-			firstname: "Please enter your firstname",
-			lastname: "Please enter your lastname",
-			username: {
-				required: "Please enter a username",
-				minlength: "Your username must consist of at least 2 characters"
-			},
-			password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long"
-			},
-			confirm_password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long",
-				equalTo: "Please enter the same password as above"
-			},
-			email: "Please enter a valid email address",
-			agree: "Please accept our policy"
-		}
-	});
-
-	// propose username by combining first- and lastname
-	$("#username").focus(function() {
-		var firstname = $("#firstname").val();
-		var lastname = $("#lastname").val();
-		if(firstname && lastname && !this.value) {
-			this.value = firstname + "." + lastname;
-		}
-	});
-
-	//code to hide topic selection, disable for demo
-	var newsletter = $("#newsletter");
-	// newsletter topics are optional, hide at first
-	var inital = newsletter.is(":checked");
-	var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
-	var topicInputs = topics.find("input").attr("disabled", !inital);
-	// show when newsletter is checked
-	newsletter.click(function() {
-		topics[this.checked ? "removeClass" : "addClass"]("gray");
-		topicInputs.attr("disabled", !this.checked);
-	});
-});
-</script>
-
-<style type="text/css">
-#commentForm { width: 500px; }
-#commentForm label { width: 250px; }
-#commentForm label.error, #commentForm input.submit { margin-left: 253px; }
-#signupForm { width: 670px; }
-#signupForm label.error {
-	margin-left: 10px;
-	width: auto;
-	display: inline;
-}
-#newsletter_topics label.error {
-	display: none;
-	margin-left: 103px;
-}
-</style>
-
-=======
-<?
-//##TODO## verify that this is a new install
-//##TODO## add menu if the DB exists
-?>
->>>>>>> master
 
 	<head>
     <title>RaspberryPints Installation</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <!-- Set location of Cascading Style Sheet -->
-<<<<<<< HEAD
+    <!-- Set location of Cascading Style Sheets and script files -->
     <link rel="stylesheet" type="text/css" href="styles.css" />
 	<link rel="shortcut icon" href="../img/pint.ico" />
-	
-	<link rel="stylesheet" href="css/screen.css" />
-	<script src="includes/jquery.js"></script>
+
+	<script type="text/javascript" src="includes/jquery-2.1.0.min.js"></script>	
 	<script src="includes/jquery.validate.js"></script>
     
-=======
-    <link rel="stylesheet" type="text/css" href="../admin/styles.css" />
-    <link rel="shortcut icon" href="../img/pint.ico" />
->>>>>>> master
   </head>
   <body>
   <h1>Welcome to Raspberry Pints!</h1>
   
-<<<<<<< HEAD
      <form action="includes/configprocessor.php" method="post">
 
 	<table border="1">
 	<tr><td>
 	<h4>What do you want to do? ##TODO - Automate this##</h4>
 	<select name="selectaction">
-=======
- 
-  
-  <h3>Installation Information</h3>
-  <br>
-  In order to get started, we'll need a little information from you. When you installed mySQL, you were asked for a "root" password.
-  You'll need to enter that here for us to configure RPints for you. You should leave the Database Server name as the default, unless
-  you are certain you need to change it.
-    <form action="includes/configprocessor.php" method="post">
-
-	<h4>What do you want to do?</h4>
-	<select name="selectaction">
-	  <option value="">Select...</option>
->>>>>>> master
 	  <option value="install">Install</option>
 	  <option value="upgrade">Upgrade</option>
 	  <option value="remove">Clear Data</option>
 	</select>
-<<<<<<< HEAD
 	</td></tr>
 	</table>
 	<br><br><br>
@@ -192,41 +68,21 @@ you are certain you need to change it.
 				</td>
 				<td>
 					<input minlength="2" required class="inputbox" value="localhost" type="text" name="servername" required>				
-=======
-	
-	<table>
-			<tr>
-				<td>
-					<label for="textfield"><strong>Database Server:</strong></label> 
-				</td>
-				<td>
-					<input class="inputbox" value="localhost" type="text" name="servername" required>				
->>>>>>> master
 				</td>
 			</tr>
 			<tr>
 				<td>
-<<<<<<< HEAD
 					<label for="textfield"><strong>Root Password: (required)</strong></label> 
 				</td>
 				<td>
 					<input class="inputbox" required type="password" name="rootpass">			
-=======
-					<label for="textfield"><strong>Root Password:</strong></label> 
-				</td>
-				<td>
-					<input class="inputbox" type="password" name="rootpass">			
->>>>>>> master
 				</td>
 			</tr>
 		</table>
 		<br />				
 		<br />
-<<<<<<< HEAD
 	<br>
 	<h3>Step<span class="tapcircle">2</span></h3>
-=======
->>>>>>> master
 		Now it's time to create the database user for Raspberry Pints to use. The default is "beers" and you can keep the default if you would like.
 		This database account is just used by the software to access the database. This is not your administration account.
 		<table>
@@ -235,11 +91,7 @@ you are certain you need to change it.
 					<label for="textfield"><strong>Database Username:</strong> 
 				</td>
 				<td>
-<<<<<<< HEAD
 					</label> <input required class="inputbox" value="beers" name="dbuser">				
-=======
-					</label> <input class="inputbox" value="beers" name="dbuser">				
->>>>>>> master
 				</td>
 			</tr>
 			<tr>
@@ -247,22 +99,14 @@ you are certain you need to change it.
 					<label for="textfield"><strong>Database Password:</strong></label>
 				</td>
 				<td>
-<<<<<<< HEAD
 					<input required class="inputbox" id="txtNewPassword" type="password" name="dbpass1">		
-=======
-					<input class="inputbox" id="txtNewPassword" type="password" name="dbpass1">		
->>>>>>> master
 				</td>
 			</tr>			<tr>
 				<td>
 					<label for="textfield"><strong>Confirm Password:</strong></label>
 				</td>
 				<td>
-<<<<<<< HEAD
 					<input required class="inputbox" id="txtConfirmPassword" type="password" name="dbpass2" onChange="checkPasswordMatch()";>
-=======
-					<input class="inputbox" id="txtConfirmPassword" type="password" name="dbpass2" onChange="checkPasswordMatch()";>
->>>>>>> master
 				</td>
 				<td>
 					<div class="registrationFormAlert" id="divCheckPasswordMatch"></div>
@@ -271,10 +115,7 @@ you are certain you need to change it.
 		</table>
 		<br />
 		<br />
-<<<<<<< HEAD
 	<h3>Step<span class="tapcircle">3</span></h3>
-=======
->>>>>>> master
 				And at last, we'll need to create a management account. this account is used for adding / removing beers, etc.
 		<table>
 			<tr>
@@ -282,11 +123,7 @@ you are certain you need to change it.
 					<label for="textfield"><strong>RPints Username:</strong> 
 				</td>
 				<td>
-<<<<<<< HEAD
 					</label> <input required class="inputbox" value="admin" name="adminuser">				
-=======
-					</label> <input class="inputbox" value="admin" name="adminuser">				
->>>>>>> master
 				</td>
 			</tr>
 			<tr>
@@ -294,11 +131,7 @@ you are certain you need to change it.
 					<label for="textfield"><strong>Email Address:</strong> 
 				</td>
 				<td>
-<<<<<<< HEAD
 					</label> <input required class="inputbox" name="adminemail">				
-=======
-					</label> <input class="inputbox" name="adminemail">				
->>>>>>> master
 				</td>
 			</tr>
 			<tr>
@@ -306,11 +139,7 @@ you are certain you need to change it.
 					<label for="textfield"><strong>Your Name:</strong> 
 				</td>
 				<td>
-<<<<<<< HEAD
 					</label> <input required class="inputbox" name="adminname">				
-=======
-					</label> <input class="inputbox" name="adminname">				
->>>>>>> master
 				</td>
 			</tr>
 			<tr>
@@ -318,22 +147,14 @@ you are certain you need to change it.
 					<label for="textfield"><strong>Admin Password:</strong></label>
 				</td>
 				<td>
-<<<<<<< HEAD
 					<input required class="inputbox" id="txtCheckAdminPassword" type="password" name="adminpass1">		
-=======
-					<input class="inputbox" id="txtCheckAdminPassword" type="password" name="adminpass1">		
->>>>>>> master
 				</td>
 			</tr>			<tr>
 				<td>
 					<label for="textfield"><strong>Confirm Password:</strong></label>
 				</td>
 				<td>
-<<<<<<< HEAD
 					<input required class="inputbox" id="txtConfirmAdminPassword" type="password" name="adminpass2" onChange="checkAdminPasswordMatch()";>
-=======
-					<input class="inputbox" id="txtConfirmAdminPassword" type="password" name="adminpass2" onChange="checkAdminPasswordMatch()";>
->>>>>>> master
 				</td>
 				<td>
 					<div class="registrationFormAlert" id="divCheckAdminPasswordMatch"></div>
