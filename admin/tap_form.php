306b85bdb429bd43a924ae43964f9afc3241cc18
Jason <JasonUnterman@gmail.com>
2014-02-06 09:15:56 -0500

    bug fix
    #100

--- admin/tap_form.php
+++ admin/tap_form.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 
-$beerList = $beerManager->GetAll();
+$beerList = $beerManager->GetAllActive();
 $kegList = $kegManager->GetAllAvailable();
 
 $tapNumber = $_GET['tapNumber'];
