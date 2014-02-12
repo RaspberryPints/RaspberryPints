a7ba75d84e8beb83343a57ef54a6afa60c2696ef
Skemp45 <Shawn@besmartdesigns.com>
2014-02-12 09:00:06 -0700

    updated to fix style issues

--- admin/includes/email_tou.php
+++ admin/includes/email_tou.php
@@ -6,7 +6,7 @@
 <link href="../styles/layout.css" rel="stylesheet" type="text/css" />
 <link href="../styles/login.css" rel="stylesheet" type="text/css" />
 <!-- Theme Start -->
-<link href="../style.css" rel="stylesheet" type="text/css" />
+<link href="../styles.css" rel="stylesheet" type="text/css" />
 <!-- Theme End -->
 
 </head>
@@ -14,12 +14,12 @@
 	<div id="logincontainer">
 		<div id="loginbox">
 			<div id="loginheader">
-				<a href="../" style="text-decoration:none;"><h1><font color="#00CCFF">RaspberryPints Login</h1></font></a>
+		<a href="../" style="text-decoration:none;"><h1><font color="#00CCFF">RaspberryPints Login</h1></font></a>
 			</div>
 			<div id="innerlogin">
 				<form name="email_tou" action="send_username_ac.php" method="post">
 				<p>Enter your Email:</p>
-					<input type="text" class="logininput" name="email_tou" id="email_tou" />
+					<input type="text" class="logininput" autofocus="autofocus" name="email_tou" id="email_tou" />
 					<input type="submit" class="loginbtn" value="Submit" name="submit" /><br />
 				</form>
 <a href="../index.php" style="text-decoration:none;"><font color="grey">Go Back To Login</font></a> 
