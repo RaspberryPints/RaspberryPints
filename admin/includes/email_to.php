9f72dc68ab2ec32c19fdda91558c7ba5bf9de441
Skemp45 <Shawn@besmartdesigns.com>
2014-02-12 09:00:54 -0700

    fixed style issues

--- admin/includes/email_to.php
+++ admin/includes/email_to.php
@@ -6,7 +6,7 @@
 <link href="../styles/layout.css" rel="stylesheet" type="text/css" />
 <link href="../styles/login.css" rel="stylesheet" type="text/css" />
 <!-- Theme Start -->
-<link href="../style.css" rel="stylesheet" type="text/css" />
+<link href="../styles.css" rel="stylesheet" type="text/css" />
 <!-- Theme End -->
 
 </head>
@@ -19,7 +19,7 @@
 			<div id="innerlogin">
 				<form name="email_to" action="send_password_ac.php" method="post">
 				<p>Enter your Email:</p>
-					<input type="text" class="logininput" name="email_to" id="email_to" />
+					<input type="text" class="logininput" autofocus="autofocus" name="email_to" id="email_to" />
 					<input type="submit" class="loginbtn" value="Submit" name="submit" /><br />
 				</form>
 <a href="../index.php" style="text-decoration:none;"><font color="grey">Go Back To Login</font></a> 
