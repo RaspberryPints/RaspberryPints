7ceb8b267e04ad80198e2227fcc8857c01d32b1a
mcangeli <mcangeli@gmail.com>
2014-02-25 15:44:17 -0500

    added code to post flow info to Database Changed p in style.css to have a padding of 5px instead of 20

--- python/flow_monitor.py
+++ python/flow_monitor.py
@@ -2,6 +2,7 @@
 import serial
 import syslog
 import time
+import MySQLdb as mdb
 
 #The following line is for serial over GPIO
 port = '/dev/ttyS0'
@@ -9,6 +10,7 @@ port = '/dev/ttyS0'
 #port = '/dev/ttyACM0'
 
 arduino = serial.Serial(port,9600,timeout=2)
+
 
 
 running = True
@@ -30,12 +32,22 @@ try:
 			print "  - Addr : "+hex(MCP_ADDR)
 			print "  - Pin  : "+str(MCP_PIN)
 			print "  - Count: "+str(POUR_COUNT)
+			con = mdb.connect('localhost','root','YEbrak4M!','raspberrypints')
+			
+			cur = con.cursor()
+			cur.execute("INSERT INTO pourData(count,pin,reading) values (%s,%s,%s)",(POUR_COUNT,MCP_PIN,reading[0]))
+			con.commit()
 		elif ( reading[0] == "K" ):
 			MCP_ADDR = int(reading[1])
 			MCP_PIN = int(reading[2])
 			print "Keg Kicked:"
 			print "  - Addr : "+hex(MCP_ADDR)
 			print "  - Pin  : "+str(MCP_PIN)
+			con = mdb.connect('localhost','root','YEbrak4M!','raspberrypints')
+			
+			cur = con.cursor()
+			cur.execute("INSERT INTO pourData(pin,reading) values (%s,%s)", (MCP_PIN,reading[0]))
+			con.commit()
 		else:
 			print "Unknown message: "+msg
 finally:
