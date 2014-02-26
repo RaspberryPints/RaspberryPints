ebe9dc35838bf3ce9cc534868bac4258ad57651d
Thadius Miller <thad@raspberrypints.com>
2014-02-25 20:02:51 -0800

    Fixing merge conflicts

---
+++ maintenance_scripts/mysql_dump.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CUR_DB_HOST=localhost
+CUR_DB_NAME=raspberrypints
+CUR_DB_USER=beers
+CUR_DB_PASS=beer
+#make MySQL backup filename
+BACKUPMYSQLFILE=/var/www/raspberry-$(date +%Y-%m-%d).sql
+#export Database
+mysqldump -h localhost -u $CUR_DB_USER -p$CUR_DB_PASS $CUR_DB_NAME > $CUR_PATH$BACKUPMYSQLFILE
