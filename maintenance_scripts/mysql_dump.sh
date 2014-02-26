9d63af0aa9ad9b4ccfbe00f86c0dd047f477d5fb
Skemp45 <Shawn@besmartdesigns.com>
2014-02-26 11:16:51 -0700

    Update mysql_dump.sh

--- maintenance_scripts/mysql_dump.sh
+++ maintenance_scripts/mysql_dump.sh
@@ -4,6 +4,6 @@ CUR_DB_NAME=raspberrypints
 CUR_DB_USER=beers
 CUR_DB_PASS=beer
 #make MySQL backup filename
-BACKUPMYSQLFILE=/var/www/raspberry-$(date +%Y-%m-%d).sql
+BACKUPMYSQLFILE=/var/www/backups/raspberry-$(date +%Y-%m-%d).sql
 #export Database
 mysqldump -h localhost -u $CUR_DB_USER -p$CUR_DB_PASS $CUR_DB_NAME > $CUR_PATH$BACKUPMYSQLFILE
