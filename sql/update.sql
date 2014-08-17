3dcbf69b3ef5de3f12325ca311a5776ca74d86cc
mcangeli <mcangeli@gmail.com>
2014-08-17 00:20:48 -0400

    Update update.sql
    Add pulses to the pours table so we have a record of the pulses (for use later in calibration/trouble shooting)

--- sql/update.sql
+++ sql/update.sql
@@ -2,3 +2,4 @@ ALTER TABLE  `pours` ADD  `pinId` INT( 11 ) NULL AFTER  `tapId`;
 ALTER TABLE  `taps` ADD  `pinId` INT( 11 ) NULL AFTER  `tapNumber`;
 INSERT INTO `raspberrypints`.`config` (`id`, `configName`, `configValue`, `displayName`, `showOnPanel`, `createdDate`, `modifiedDate`) VALUES (NULL, 'useFlowMeter', '0', 'Use Flow Monitoring', '1', NOW(), NOW());
 ALTER TABLE `pours` CHANGE `amountPoured` `amountPoured` FLOAT( 6, 3 ) NOT NULL;
+ALTER TABLE  `pours` ADD  `pulses` INT( 6 ) NOT NULL AFTER  `amountPoured`;
