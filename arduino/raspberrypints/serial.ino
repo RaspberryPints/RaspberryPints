226cf5d06ebdcb22a29b526d4169c7892ddca957
CaptnDelta <thomas@hentschel.net>
2015-10-03 19:42:55 -0700

    forgot one alamode file, this goes with the last commit...

--- arduino/raspberrypints/serial.ino
+++ arduino/raspberrypints/serial.ino
@@ -7,6 +7,15 @@ void sendPulseCount(uint8_t addr, int pinNum, unsigned int pulseCount) {
   Serial.println(pulseCount);
 }
 
+void sendUpdateCount(uint8_t addr, int pinNum, unsigned int count) {
+  Serial.print("U;");
+  Serial.print(addr);
+  Serial.print(";");
+  Serial.print(pinNum);
+  Serial.print(";");
+  Serial.println(count);
+}
+
 void sendKickedMsg(uint8_t addr, int pinNum) {
   Serial.print("K;");
   Serial.print(addr);
