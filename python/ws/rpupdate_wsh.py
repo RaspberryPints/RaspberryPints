20e6a8ecfffe6654974aa1305b5fa4fcc36da107
CaptnDelta <thomas@hentschel.net>
2015-10-13 00:28:04 -0700

    license update

--- python/ws/rpupdate_wsh.py
+++ python/ws/rpupdate_wsh.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42 3/4):
+# <thomas@hentschel.net> wrote this file. As long as you retain this notice you
+#  can do whatever you want with this stuff. If we meet some day, and you think
+#  this stuff is worth it, you can buy me a beer in return 
+# -Th
+#  ----------------------------------------------------------------------------
+  
 import socket
 import struct
 import select
