f6b78bd50cd7ea06fefc6414fff7b2886d0014ba
CaptnDelta <thomas@hentschel.net>
2015-10-14 21:59:57 -0700

    Restoring license (stepped on in last commit)

--- python/PintDispatch.py
+++ python/PintDispatch.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
-
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42 3/4):
+# <thomas@hentschel.net> wrote this file. As long as you retain this notice you
+#  can do whatever you want with this stuff. If we meet some day, and you think
+#  this stuff is worth it, you can buy me a beer in return 
+# -Th
+#  ----------------------------------------------------------------------------
+  
 import threading
 import time
 import signal
