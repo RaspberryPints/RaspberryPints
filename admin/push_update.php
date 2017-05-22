29b3049062e95a193f65973bca10cb71246236ec
Eric Young <eric@aceshome.com>
2017-05-22 12:07:50 -0400

    a og->abv change I missed

--- admin/push_update.php
+++ admin/push_update.php
@@ -20,7 +20,7 @@ $beerid=$_POST['beerid'];
 
 
 // update data in mysql database
-$sql="UPDATE beers SET name='$name', style='$style', notes='$notes', og='$og', fg='$fg', srm='$srm',
+$sql="UPDATE beers SET name='$name', style='$style', notes='$notes', abv='$abv', srm='$srm',
 ibu='$ibu', active='$active', tapnumber='$tapnumber' WHERE beerid='$beerid'";
 $result=mysql_query($sql);
 
