c8767b650e2d5bfd2ada53d3d696335f00c9d79d
rtlindne <40405221+rtlindne@users.noreply.github.com>
2018-11-13 09:25:01 -0600

    Changed refresh.sh to loop through all users on the pi to see if any are running chromium that needs to be refreshed (in case default user pi is not used)

--- includes/refresh.sh
+++ includes/refresh.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export DISPLAY=":0.0"
-WID=$(xdotool search --onlyvisible --class chromium|head -1)
-if [ $WID ]; then
-	xdotool windowactivate ${WID}
-	xdotool key ctrl+F5
-fi
+for dir in /home/*/
+do
+	export XAUTHORITY=$dir.Xauthority
+	WID=$(xdotool search --onlyvisible --class chromium|head -1)
+	if [ "$WID" != "" ]; then
+		xdotool windowactivate ${WID}
+		xdotool key ctrl+F5
+	fi
+done
