1179cf938502b656786c2a30ecae28610ac79ea9
rtlindne <40405221+rtlindne@users.noreply.github.com>
2019-01-24 15:05:57 -0600

    Updated brewery_manager to have more helper functions and have its save match manager save

--- admin/includes/managers/brewery_manager.php
+++ admin/includes/managers/brewery_manager.php
@@ -21,13 +21,46 @@ class BreweryManager extends Manager{
 	protected function hasModifiedColumn(){return false;}
 	protected function hasCreatedColumn(){return false;}
 
-	function Save($brewery){
+	function Save($brewery, $new=false){
 		// download the image so we have it locally
 		if ($brewery->get_imageUrl() && filter_var($brewery->get_imageUrl(), FILTER_VALIDATE_URL) ) {
-			$imagepath =  "../data/images/".uniqid("brewery-").".png";
+			$imagepath =  __DIR__."../../includes/cache/img/".uniqid("brewery-").".png";
 			copy($brewery->get_imageUrl(), $imagepath);
 			if( file_exists($imagepath) ) $brewery->set_imageUrl($imagepath);
 		}
-		return parent::Save($brewery);
+		return parent::Save($brewery, $new);
+	}
+	
+	function GetOrAdd($name, $url){
+	    $brewery = null;
+	    if(($brewery = $this->getByName($name))) {
+	        return $brewery;
+	    }elseif(($brewery = $this->getByURL($url))){
+	        return $brewery;
+	    }
+	        
+        $brewery = new Brewery();
+        $brewery->set_name($name);
+        $brewery->set_imageUrl($url);
+        $this->Save($brewery);
+        
+        $brewery = $this->getByName($name);
+    
+	    
+	    return $brewery;
+	}
+	function getByName($name){
+	    global $mysqli;
+	    $sql="SELECT * FROM ".$this->getViewName()." WHERE name = '".$mysqli->escape_string($name)."' ".$this->getOrderByClause();
+	    $results = $this->executeQueryWithResults($sql);
+	    if( $results && count($results) > 0 ) return array_values($results)[0];
+	    return null;	    
+	}
+	function getByURL($url){
+	    global $mysqli;
+	    $sql="SELECT * FROM ".$this->getViewName()." WHERE imageUrl = '".$mysqli->escape_string($url)."' ".$this->getOrderByClause();
+	    $results = $this->executeQueryWithResults($sql);
+	    if( $results && count($results) > 0 ) return array_values($results)[0];
+	    return null;	    
 	}
 }
