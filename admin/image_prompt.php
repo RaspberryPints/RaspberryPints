f1e23c10f9e80fef51cdaf588ca07531f87fbb9f
rtlindne <40405221+rtlindne@users.noreply.github.com>
2020-08-05 10:26:51 -0500

    Allows image_prompt to pass redirect to upload_image and added redirect to image_remove

--- admin/image_prompt.php
+++ admin/image_prompt.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/header.php';
     <form enctype="multipart/form-data" action="includes/upload_image.php" method="POST">
         <input name="uploaded" type="file" accept="image/gif, image/jpg, image/png"/>
         <input type="hidden" name="target" value="<?php echo __DIR__.'/../img/'.(isset($_POST["targetDir"])?$_POST["targetDir"]:'tap/tap').$_GET['id'] ?>"/>
-        <input type="hidden" name="redirect" value=""/>
+        <input type="hidden" name="redirect" value="<?php echo $_POST["redirect"]?>"/>
         <input type="hidden" name="deleteOthers" value=""/>
         <input type="submit" class="btn" value="Upload" /><br /><br />'
     </form>'
