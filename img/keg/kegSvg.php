8bff775d8c1156b046ffa7bfd32d83380b44d0f8
rtlindne <40405221+rtlindne@users.noreply.github.com>
2020-09-16 18:06:12 -0500

    added check for RGB set

--- img/keg/kegSvg.php
+++ img/keg/kegSvg.php
@@ -19,7 +19,7 @@ $view="10 00 200 300";
   <linearGradient id="rgbToTransVerticle" x2="0" y2="1">
     <stop offset="0%" stop-color="rgba(<?php echo "0,0,0,0" ?>)" />
     <stop offset="<?php echo isset($_GET['fill'])?strval(100-intval($_GET['fill'])):"0"; ?>%" stop-color="rgba(<?php echo "0,0,0,0" ?>)" />
-    <stop offset="<?php echo isset($_GET['fill'])?strval(100-intval($_GET['fill'])):"0"; ?>%" stop-color="rgb(<?php echo $_GET['rgb'] ?>)" />
+    <stop offset="<?php echo isset($_GET['fill'])?strval(100-intval($_GET['fill'])):"0"; ?>%" stop-color="rgb(<?php echo isset($_GET['rgb'])?$_GET['rgb']:"0,0,0" ?>)" />
   </linearGradient>
 <style type="text/css"><![CDATA[
   #outline {
