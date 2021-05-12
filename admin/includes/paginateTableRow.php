5e3538beda48d5d826cf775847eacfec383dd5d3
rtlindne <rtlindne>
2021-05-12 12:17:48 -0500

    Fixed pagination enter key press

--- admin/includes/paginateTableRow.php
+++ admin/includes/paginateTableRow.php
@@ -1,9 +1,13 @@
     <tr>
         <td colspan="2"><table style="vertical-align:center;">
             <tr>
+            <?php /** @var mixed $page */ ?>
+            <?php /** @var mixed $maxPage */ ?>
+            <?php /** @var mixed $columnCount */ ?>
+            <?php /** @var mixed $totalRows */ ?>
             	<td style="width:60px"><?php if($page > 1){?><input type="button" name="firstPage" value="&lt;&lt;" onclick="changePage(<?php echo 1; ?>);"/><input type="button" name="lastPage" value="&lt;" onclick="changePage(<?php echo $page - 1?>);"><?php } ?></td>
                 <td>
-                	<input style="width:50px" <?php echo ($maxPage<=1?"disabled":"");?> type="text" name="page" value="<?php echo $page; ?>" onkeypress="if(e.keyCode==13)$(statForm).submit();"/>:<?php echo $maxPage; ?>
+                	<input style="width:50px" <?php echo ($maxPage<=1?"disabled":"");?> type="text" name="page" value="<?php echo $page; ?>" onkeypress="javascript: if(event.keyCode==13){changePage(this.value);$(statForm).submit();}"/>:<?php echo $maxPage; ?>
                 	<input type="hidden" name="maxPage" value="<?php echo $maxPage; ?>">
                 	<input type="hidden" name="totalRows" value="<?php echo $totalRows; ?>">
                 	<input type="hidden" name="queryChanged" value="0">
