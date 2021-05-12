8f0a5f9d7d2c1ad9ce51f13b3960e8d9eda15ce8
rtlindne <rtlindne>
2021-05-12 14:37:42 -0500

    UPdated login to return to the last page

--- admin/header.php
+++ admin/header.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset( $_SESSION['myusername'] )){
-	header("location:index.php");
+    header("location:index.php?ret=".urlencode($_SERVER['REQUEST_URI']));
 }
 require_once __DIR__.'/includes/conn.php';
 require_once __DIR__.'/includes/html_helper.php';
@@ -35,10 +35,11 @@ require_once __DIR__.'/includes/managers/tap_manager.php';
 <link href="styles/layout.css" rel="stylesheet" type="text/css" />
 <link href="styles/wysiwyg.css" rel="stylesheet" type="text/css" />
     <!-- Theme Start -->
-<link href="<?php echo $stylesheet?>" rel="stylesheet" type="text/css" />
+
+<link href="<?php /** @var mixed $stylesheet */echo $stylesheet?>" rel="stylesheet" type="text/css" />
     <!-- Theme End -->
 <link href='http://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
 <?php require __DIR__.'/scripts.php'; ?>
-<?php if(!isset($noHeadEnd) || !$noHeadEnd){?>
+<?php /** @var mixed $noHeadEnd */ if(!isset($noHeadEnd) || !$noHeadEnd){?>
 </head>
 <?php }?>
