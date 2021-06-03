b302b75052608d4a6e15010d71267b120d48b13f
rtlindne <rtlindne>
2021-06-03 09:29:18 -0500

    Customize tap list page update for horizontal direction
    Now table headers allow dragging columns left and right and positioning
    the data row columns correctly

--- admin/customize_tap_display.php
+++ admin/customize_tap_display.php
@@ -3,6 +3,11 @@
 $noHeadEnd = True;
 require_once __DIR__.'/header.php';
 require_once __DIR__.'/../includes/common.php';
+
+
+$config = getAllConfigs();
+//Force to verticle until horizontal is working
+$config[ConfigNames::ShowVerticleTapList] = "0";
 ?>
 <link rel="stylesheet" type="text/css" href="../style.css">
 <style>
@@ -19,14 +24,31 @@ html, body {
 }
 ul, li {list-style: none; padding: 0; margin: 0;}
 tr { border:thick; border-color: #FF0000; }
+
+<!--th {background: transparent; text-shadow: 1px 1px 1px #fff; font-size: 14px;}-->
+<?php if($config[ConfigNames::ShowVerticleTapList] == "0"){?>
+.draggable {
+        cursor: move;
+        user-select: none;
+    }
+    .placeholder {
+        border: 2px dashed #cbd5e0;
+    }
+    .clone-list {
+        
+        border-top: 1px solid #ccc;
+        display: flex;
+    }
+    .dragging {
+        border: 1px solid #ccc;
+        z-index: 999;
+    }
+<?php } ?>
 </style>
 </head>
 <?php
 
 $htmlHelper = new HtmlHelper();
-$config = getAllConfigs();
-//Force to verticle until horizontal is working
-$config[ConfigNames::ShowVerticleTapList] == "1";
 $error = FALSE;
 if (isset ( $_POST ['save'] )) {
     /** @var mixed $value **/
@@ -82,23 +104,23 @@ include 'top_menu.php';
             </div>
             <div>
     			<?php
-//     			$result = array(getConfig(ConfigNames::ShowVerticleTapList));
-// 				foreach($result as $row) {
-// 					$options = array();
-// 					$options[0] = 'On';
-// 					$options[1] = 'Off';
-// 					$validation = $row['validation'];
-// 					if( $validation !== NULL && $validation != ''){
-// 					    $valids = explode('|', $validation);
-// 					    for( $i = 0; $i < count($valids); $i++ ){
-// 					        $options[$i] = $valids[$i];
-// 					    }
-// 					}
-// 					echo '<h3>' . $row['displayName'] . ":"  ./* '<span id="' . $row['configName'] . 'Success" style="display:none; color: #8EA534;"> (Updated)</span>'.  */'</h3>'.
-//     					$options[0].'<input type="radio" ' . ($row['configValue']?'checked':'') . ' name="' . $row['configName'] . '" value="1">' .
-//     					$options[1].'<input type="radio" ' . (!$row['configValue']?'checked':'') . ' name="' . $row['configName'] . '" value="0">'.
-//     					'<br><br>';
-//     				}
+    			$result = array(getConfig(ConfigNames::ShowVerticleTapList));
+				foreach($result as $row) {
+					$options = array();
+					$options[0] = 'On';
+					$options[1] = 'Off';
+					$validation = $row['validation'];
+					if( $validation !== NULL && $validation != ''){
+					    $valids = explode('|', $validation);
+					    for( $i = 0; $i < count($valids); $i++ ){
+					        $options[$i] = $valids[$i];
+					    }
+					}
+					echo '<h3>' . $row['displayName'] . ":"  ./* '<span id="' . $row['configName'] . 'Success" style="display:none; color: #8EA534;"> (Updated)</span>'.  */'</h3>'.
+    					$options[0].'<input type="radio" ' . ($row['configValue']?'checked':'') . ' name="' . $row['configName'] . '" value="1">' .
+    					$options[1].'<input type="radio" ' . (!$row['configValue']?'checked':'') . ' name="' . $row['configName'] . '" value="0">'.
+    					'<br><br>';
+    				}
     			?>        
             </div>
 	        
@@ -108,10 +130,10 @@ include 'top_menu.php';
                 $beeritem = array(
                     "id" => 1,
                     "beername" => "BeerName",
-                    "untID" => 0,
-                    "style" => 1,
+                    "untID" => 4064135,
+                    "style" => 'Style',
                     "brewery" => "BreweryName",
-                    "breweryImage" =>'breweryImageUrl',
+                    "breweryImage" =>'https://untappd.akamaized.net/site/assets/images/temp/badge-brewery-default.png',
                     "notes" => "Beer Notes",
                     "abv" => 5.0,
                     "og" => 1.065,
@@ -128,13 +150,18 @@ include 'top_menu.php';
                     "tapNumber" => 1,
                     "rating" => 4.5,
                     "srmRgb" => 2,
-                    "valvePinState" => 0
+                    "containerType" => 'standardpint',
+                    "kegType" => 'corny',
+                    "valvePinState" => 0,
+                    "accolades" => '1~Gold~1,2~Silver~1'
                 );
                 $taps[1] = $beeritem;
                 $numberOfTaps = 1;
                 /** @var mixed $numberOfBeers **/
                 $numberOfBeers = 1;
+                echo '<table><tr><td>';
                 printBeerList($taps, $numberOfTaps, ConfigNames::CONTAINER_TYPE_KEG, TRUE);
+                echo '</td></tr></table>';
             ?>
             </div>
 			<div>
@@ -152,31 +179,293 @@ include 'top_menu.php';
 <script>
 <?php if($config[ConfigNames::ShowVerticleTapList] == "0"){?>
 //Sortable column heads
-var oldIndex;
-/* $('tr').custSortable({
-  containerSelector: 'tr',
-  itemSelector: 'th',
-  placeholder: '<th class="placeholder"/>',
-  vertical: false,
-    stop: function( event, ui ) {
-    	 allInputs = $('form').find('input[type=hidden]');
-    	 for( i = 0; i < allInputs.length; i++ ){
-    	 	allInputs[i].value = (i+1);
-    	 }
-    	}
-}); */
-$('tbody').sortableColumn({
-	axis:"x",
-	stop: function( event, ui ) {
-		 allInputs = $('form').find('input[type=hidden]');
-		 for( i = 0; i < allInputs.length; i++ ){
-		 	allInputs[i].value = (i+1);
-		 }
-		},
-	items: function( element, event ) {
-			return $('td,th');
-		}
-	}); 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const table = document.getElementById('beerList');
+
+    let draggingEle;
+    let draggingColumnIndex;
+    let placeholder;
+    let list;
+    let isDraggingStarted = false;
+
+    // The current position of mouse relative to the dragging element
+    let x = 0;
+    let y = 0;
+    // Swap two nodes
+    const swap = function(nodeA, nodeB) {
+        const parentA = nodeA.parentNode;
+        const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+
+        // Move `nodeA` to before the `nodeB`
+        nodeB.parentNode.insertBefore(nodeA, nodeB);
+
+        // Move `nodeB` to before the sibling of `nodeA`
+        parentA.insertBefore(nodeB, siblingA);
+    };
+
+    // Check if `nodeA` is on the left of `nodeB`
+    const isOnLeft = function(nodeA, nodeB) {
+        // Get the bounding rectangle of nodes
+        const rectA = nodeA.getBoundingClientRect();
+        const rectB = nodeB.getBoundingClientRect();
+
+        return (rectA.left + rectA.width / 2 < rectB.left + rectB.width / 2);
+    };
+
+    const cloneTable = function() {
+        const rect = table.getBoundingClientRect();
+
+        list = document.createElement('div');
+        list.classList.add('clone-list');
+        list.style.position = 'absolute';
+        list.style.left = `${rect.left}px`;
+        list.style.top = `${rect.top}px`;
+        table.parentNode.insertBefore(list, table);
+
+        // Hide the original table
+        table.style.visibility = 'hidden';
+
+        // Get all cells
+        originalCells = [].slice.call(table.querySelectorAll('#beerList tbody td'));
+        originalCells = originalCells.filter(function(c, idx) {
+            return getNearestTableAncestor(c,'tbody') === table.children[0];
+        });
+        const originalHeaderCells = [].slice.call(table.querySelectorAll('th'));
+        const numColumns = originalCells.length;
+
+        // Loop through the header cells
+        originalHeaderCells.forEach(function(headerCell, headerIndex) {
+            const width = parseInt(window.getComputedStyle(headerCell).width);
+            const height = parseInt(window.getComputedStyle(headerCell).height);
+
+            // Create a new table from given row
+            const item = document.createElement('div');
+            item.classList.add('draggable');
+
+            const newTable = document.createElement('table');
+            newTable.setAttribute('class', 'clone-table');
+            newTable.setAttribute('class', 'clone-table');
+            newTable.style.width = `${width}px`;
+
+            // Header
+            const th = headerCell.cloneNode(true);
+            th.innerHTML = th.innerHTML.substring(0,th.innerHTML.indexOf('<br>'))
+            let newRow = document.createElement('tr');
+            th.style.height = `${height}px`;
+            newRow.appendChild(th);
+            newTable.appendChild(newRow);
+
+            const cells = originalCells.filter(function(c, idx) {
+                return thFromTd($(c))[0] === headerCell;
+            });
+            newRow = document.createElement('tr');
+            cells.forEach(function(cell) {
+                const newCell = cell.cloneNode(true);
+                newCell.style = cell.style;
+                newCell.style.width = `${width}px`;
+                newCell.style.borderLeft = '1px white dashed';
+                newRow.appendChild(newCell);
+            });
+            newTable.appendChild(newRow);
+
+            item.appendChild(newTable);
+            list.appendChild(item);
+        });
+    };
+    const getNearestTableAncestor = function (htmlElementNode, element) {
+        while (htmlElementNode) {
+            htmlElementNode = htmlElementNode.parentNode;
+            if (htmlElementNode.tagName.toLowerCase() === element) {
+                return htmlElementNode;
+            }
+        }
+        return undefined;
+    }
+    const mouseDownHandler = function(e) {
+        if(e.target.tagName=="INPUT")return;
+        draggingColumnIndex = [].slice.call(table.querySelectorAll('th')).indexOf(e.target);
+        if(draggingColumnIndex<0)draggingColumnIndex = [].slice.call(table.querySelectorAll('td')).indexOf(e.target);
+
+        // Determine the mouse position
+        x = e.clientX - e.target.offsetLeft;
+        y = e.clientY - e.target.offsetTop;
+
+        // Attach the listeners to `document`
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function(e) {
+        if (!isDraggingStarted) {
+            isDraggingStarted = true;
+
+            cloneTable();
+
+            draggingEle = [].slice.call(list.children)[draggingColumnIndex];
+            draggingEle.classList.add('dragging');
+
+            // Let the placeholder take the height of dragging element
+            // So the next element won't move to the left or right
+            // to fill the dragging element space
+            placeholder = document.createElement('div');
+            placeholder.classList.add('placeholder');
+            draggingEle.parentNode.insertBefore(placeholder, draggingEle.nextSibling);
+            placeholder.style.width = `${draggingEle.offsetWidth}px`;
+        }
+
+        // Set position for dragging element
+        draggingEle.style.position = 'absolute';
+        draggingEle.style.top = `${draggingEle.offsetTop + e.clientY - y}px`;
+        draggingEle.style.left = `${draggingEle.offsetLeft + e.clientX - x}px`;
+
+        // Reassign the position of mouse
+        x = e.clientX;
+        y = e.clientY;
+
+        // The current order
+        // prevEle
+        // draggingEle
+        // placeholder
+        // nextEle
+        const prevEle = draggingEle.previousElementSibling;
+        const nextEle = placeholder.nextElementSibling;
+        
+        // // The dragging element is above the previous element
+        // // User moves the dragging element to the left
+        if (prevEle && isOnLeft(draggingEle, prevEle)) {
+            // The current order    -> The new order
+            // prevEle              -> placeholder
+            // draggingEle          -> draggingEle
+            // placeholder          -> prevEle
+            swap(placeholder, draggingEle);
+            swap(placeholder, prevEle);
+            return;
+        }
+
+        // The dragging element is below the next element
+        // User moves the dragging element to the bottom
+        if (nextEle && isOnLeft(nextEle, draggingEle)) {
+            // The current order    -> The new order
+            // draggingEle          -> nextEle
+            // placeholder          -> placeholder
+            // nextEle              -> draggingEle
+            swap(nextEle, placeholder);
+            swap(nextEle, draggingEle);
+        }
+    };
+
+    const mouseUpHandler = function() {
+        // // Remove the placeholder
+        placeholder && placeholder.parentNode.removeChild(placeholder);
+        
+        draggingEle.classList.remove('dragging');
+        draggingEle.style.removeProperty('top');
+        draggingEle.style.removeProperty('left');
+        draggingEle.style.removeProperty('position');
+
+        // Get the end index
+        endColumnIndex = [].slice.call(list.children).indexOf(draggingEle);
+        isDraggingStarted = false;
+
+        // Remove the `list` element
+        list.parentNode.removeChild(list);
+
+        // Move the dragged column to `endColumnIndex`
+        allRows = [].slice.call(table.querySelectorAll('tr'));
+        allRows = allRows.filter(function(r, idx) {
+            return getNearestTableAncestor(r,'tbody') === table.children[0];
+        });
+        colSpanTh = 0;
+        crossColSpanTh = 0;
+        startThIndex = 0;
+        allRows.forEach(function(row) {
+            cells = [].slice.call(row.querySelectorAll('th, td'));
+            cells = cells.filter(function(c, idx) {
+                return getNearestTableAncestor(c,'tbody') === table.children[0];
+            });
+            //TH row is the row that indices are based off of
+            if(cells[draggingColumnIndex].tagName=="TH") {
+            	colSpanTh = cells[draggingColumnIndex].colSpan;
+                for(i=0;i<=Math.max(draggingColumnIndex,endColumnIndex); i++)
+                {
+                    if(i<draggingColumnIndex)startThIndex += cells[i].colSpan-1;
+                	if(i >= (Math.min(draggingColumnIndex,endColumnIndex)+(draggingColumnIndex > endColumnIndex?0:1)) && 
+                       i <  (Math.max(draggingColumnIndex,endColumnIndex)+(draggingColumnIndex > endColumnIndex?0:1)))crossColSpanTh += cells[i].colSpan;
+                }
+                row.insertBefore(cells[draggingColumnIndex], draggingColumnIndex > endColumnIndex?cells[endColumnIndex]:cells[endColumnIndex].nextSibling);
+            }else{
+            	draggingColumnIndexTd = 0;
+            	i = 0;
+                crossColSpanTd = 0;
+                while(crossColSpanTd < draggingColumnIndex+startThIndex)
+                {
+                	crossColSpanTd += cells[i++].colSpan;
+                	draggingColumnIndexTd++;
+                }
+                //when left to right add 1 because we are inserting before
+            	endColumnIndexTd = (draggingColumnIndex > endColumnIndex?draggingColumnIndexTd-(crossColSpanTh):draggingColumnIndexTd+crossColSpanTh+(colSpanTh-1)+1);
+            	endColumnIndexTd = Math.max(endColumnIndexTd,0);
+            	 
+                //For TD we are going to jump the number of columns the header jump because the array does not account for column spans we need to cjump
+                //Because cells variable does not update after each move we can use the same indexes of the array to get the moved cell
+                //I.e. index 1 is index 1 after the cell is inserted before index 5.
+                for(i=0; i<colSpanTh; i++)
+                {
+                	row.insertBefore(cells[draggingColumnIndex+startThIndex+i], (i==0?(endColumnIndexTd>=cells.length?null:cells[endColumnIndexTd]):cells[draggingColumnIndexTd+i-1].nextSibling));
+//                     draggingColumnIndex > endColumnIndex //IF moving right to left
+//                     ? cells[endColumnIndex].parentNode.insertBefore(cells[draggingColumnIndex+i-((draggingColumnIndex-endColumnIndex)-crossColSpan)], cells[endColumnIndex])
+//                     : cells[draggingColumnIndex+crossColSpan-1].parentNode.insertBefore(cells[draggingColumnIndex+i], cells[(i==0?(draggingColumnIndex+crossColSpan-1):(draggingColumnIndex+i-1))].nextSibling);
+	           }
+           }
+        });
+
+        // Bring back the table
+        table.style.removeProperty('visibility');
+
+        // Remove the handlers of `mousemove` and `mouseup`
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+        
+        allInputs = $('form').find('input[type=hidden]');
+            for( i = 0; i < allInputs.length; i++ ){
+            allInputs[i].value = (i+1);
+        }
+    };
+
+    table.querySelectorAll('th').forEach(function(headerCell) {
+        headerCell.classList.add('draggable');
+        headerCell.addEventListener('mousedown', mouseDownHandler);
+    });
+
+      // Returns jquery object
+      const thFromTd = function (td) {
+        var ofs = td.offset().left,
+            table = td.closest('table'),
+            thead = table.children('tbody').eq(0),
+            positions = cacheThPositions(thead),
+            matches = positions.filter(function(eldata) {
+              return eldata.left <= ofs;
+            }),
+            match = matches[matches.length-1],
+            matchEl = $(match.el);
+        return matchEl;
+      }
+    
+      const cacheThPositions = function (thead) {
+        allth = thead.children('tr').children('th');
+        var data = allth.map(function() {
+          var th = $(this);
+          return {
+            el: this,
+            left: th.offset().left
+          };
+        }).toArray();
+        thead.data('cached-pos', data);
+        return data;
+      }
+      //cloneTable();
+});
 <?php }else{?>
 $('tbody').sortable({
 	axis:"y",
@@ -216,7 +505,7 @@ include 'left_bar.php';
 	<!-- End Left Bar Menu -->
 	<!-- Start Js  -->
 <?php
-include 'scripts.php';
+//include 'scripts.php';
 ?>
 	<!--[if IE 6]>
 	<script type='text/javascript' src='scripts/png_fix.js'></script>
